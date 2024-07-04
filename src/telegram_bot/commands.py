51bdc3d010127545fad729d5c8f5b25b33a3a6ca
Olga Kopaeva <olya.ant@gmail.com>
2024-07-04 07:53:56 +0300

    version 1.0.1

--- src/telegram_bot/commands.py
+++ src/telegram_bot/commands.py
@@ -3,8 +3,12 @@ from aiogram.types import Message
 from aiogram.methods.send_poll import SendPoll
 
 from telegram_bot.telegram_bot import dp, bot
- 
+
 
 @dp.message(CommandStart())
-async def handler_command_start(message: Message) -> None:    
-    await message.answer(text='Когда придешь на занятие? =)')
+async def handler_command_start(message: Message) -> None:
+    await message.answer(text='Привет! Хочешь научиться танцевать? Тогда ты зашел по адресу! https://vk.com/flowkitchen')
+
+@dp.message(Command='/any_text')
+async def handler_text(message: Message) -> None:
+    await message.answer(text='Я обязательно передам сообщение Шефу, и он вам ответит.')
