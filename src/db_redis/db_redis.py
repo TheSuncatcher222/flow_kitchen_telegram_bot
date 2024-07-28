dc8b727e0cd182ac7dd591dbd5017a075a276d4d
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2024-07-28 13:46:37 +0300

    Delete old

--- src/db_redis/db_redis.py
+++
@@ -1,8 +0,0 @@
-from redis import Redis
-
-db_redis = Redis(
-    host='bot-telegram-redis',
-    port=6379,
-    db=0,
-    decode_responses=True,
-)
--- src/telegram_bot/telegram_bot.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Bot, Dispatcher
-
-from config.config import BOT_TOKEN
-
-dp = Dispatcher()
-
-bot = Bot(token=BOT_TOKEN)
