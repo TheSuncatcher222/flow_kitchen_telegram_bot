5f4926604d5c83e2d32b4c54dd839cf48fa6f3af
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2024-07-31 03:29:34 +0300

    Add PollSyncCrud, retrieve_by_title

--- app/src/crud/sync_crud/poll_sync_crud.py
+++ app/src/crud/sync_crud/poll_sync_crud.py
@@ -1,9 +1,25 @@
+from sqlalchemy.sql import select
+from sqlalchemy.sql.selectable import Select
+
 from app.src.crud.sync_crud.base_sync_crud import BaseSyncCrud
+from app.src.database.database import Base
 from app.src.models.poll import Poll
 
 
 class PollSyncCrud(BaseSyncCrud):
-    pass
+
+    def retrieve_by_title(
+        self,
+        *,
+        obj_title: str,
+        session: any,
+    ) -> Poll | None:
+        """
+        Получает один объект из базы данных по названию.
+        """
+        query: Select = select(self.model).where(self.model.title == obj_title)
+        result: list[Base] = (session.execute(query)).scalars().first()
+        return result
 
 
 poll_sync_crud: PollSyncCrud = PollSyncCrud(
