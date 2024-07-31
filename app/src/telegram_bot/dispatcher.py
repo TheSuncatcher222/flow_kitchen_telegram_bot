c8f6efd57506d2b93c5d8fb3fe182a7dac1e6e90
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2024-07-31 04:08:08 +0300

    Register chat_control_router

--- app/src/telegram_bot/dispatcher.py
+++ app/src/telegram_bot/dispatcher.py
@@ -5,6 +5,7 @@ from aiogram import (
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from app.src.telegram_bot.routers.add_poll import router as add_poll_router
+from app.src.telegram_bot.routers.chat_control import router as chat_control_router
 from app.src.telegram_bot.routers.my_polls import router as my_polls_router
 from app.src.telegram_bot.routers.start import router as start_router
 
@@ -14,6 +15,7 @@ dp: Dispatcher = Dispatcher(
 
 routers: list[Router] = (
     add_poll_router,
+    chat_control_router,
     my_polls_router,
     start_router,
 )
