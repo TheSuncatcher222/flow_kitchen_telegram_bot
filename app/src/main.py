3abb5edc0579391cf465303861d62de41cdaed5a
Kirill Svidunovich <svidunovich.spb@picassodg.ru>
2024-08-28 09:15:07 +0300

    Add redis_delete all keys in startapp

--- app/src/main.py
+++ app/src/main.py
@@ -7,11 +7,18 @@ import sys
 #       использования абсолютных путей импорта данных из модулей.
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
+from app.src.database.database import RedisKeys
 from app.src.telegram_bot.bot import bot
 from app.src.telegram_bot.dispatcher import dp
+from app.src.utils.redis_data import redis_delete
 
 
 async def main():
+    for key in (
+        RedisKeys.POLL_ALL,
+        RedisKeys.CHAT_ALL_TITLES,
+    ):
+        redis_delete(key=key)
     await dp.start_polling(bot)
 
 
