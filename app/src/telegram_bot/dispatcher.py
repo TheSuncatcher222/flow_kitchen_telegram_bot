c8e7b302e82c52f8bc396c6dd68ce6431ab9ca4c
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-04-29 15:54:33 +0300

    Re-register all routers, refactor

--- app/src/telegram_bot/dispatcher.py
+++ app/src/telegram_bot/dispatcher.py
@@ -4,20 +4,31 @@ from aiogram import (
 )
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from app.src.telegram_bot.routers.add_poll import router as add_poll_router
-from app.src.telegram_bot.routers.chat_control import router as chat_control_router
-from app.src.telegram_bot.routers.my_polls import router as my_polls_router
-from app.src.telegram_bot.routers.start import router as start_router
+from app.src.telegram_bot.routers.chat_control import router as chat_control
+from app.src.telegram_bot.routers.course_add import router as course_add
+from app.src.telegram_bot.routers.course_main import router as course_main
+from app.src.telegram_bot.routers.course_my import router as course_my
+from app.src.telegram_bot.routers.fallback import router as _fallback
+from app.src.telegram_bot.routers.poll_add import router as poll_add
+from app.src.telegram_bot.routers.poll_my import router as poll_my
+from app.src.telegram_bot.routers.redis_clear import router as redis_clear
+from app.src.telegram_bot.routers.start import router as start
 
 dp: Dispatcher = Dispatcher(
     storage=MemoryStorage(),
 )
 
 routers: list[Router] = (
-    add_poll_router,
-    chat_control_router,
-    my_polls_router,
-    start_router,
+    chat_control,
+    course_add,
+    course_my,
+    poll_add,
+    course_main,
+    poll_my,
+    redis_clear,
+    start,
+    # WARNING! Роутер _fallback должен быть зарегистрирован ниже всех остальных!
+    _fallback,
 )
 for router in routers:
     dp.include_router(router)
