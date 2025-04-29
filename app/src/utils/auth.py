ec3f908ed49eaf8443485a5fe8fd7674d44e472e
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-04-29 15:37:22 +0300

    Add IsAdmin, IsDeveloper, check_if_user_is_developer

--- app/src/utils/auth.py
+++ app/src/utils/auth.py
@@ -1,10 +1,36 @@
+from aiogram.filters import Filter
+from aiogram.types import Message
+
 from app.src.config.config import settings
 
 
-def check_if_user_is_admin(user_id: int | str) -> bool:
+class IsAdmin(Filter):
+    """
+    Фильтр доступа для администраторов.
+    """
+
+    async def __call__(self, message: Message) -> bool:
+        return check_if_user_is_admin(user_id_telegram=message.from_user.id)
+
+
+class IsDeveloper(Filter):
+    """
+    Фильтр доступа для разработчиков.
+    """
+
+    async def __call__(self, message: Message) -> bool:
+        return check_if_user_is_developer(user_id_telegram=message.from_user.id)
+
+
+def check_if_user_is_admin(user_id_telegram: int | str) -> bool:
     """
     Проверяет, является ли пользователь администратором.
     """
-    if str(user_id) in settings.BOT_ADMIN_IDS.split(','):
-        return True
-    return False
+    return str(user_id_telegram) in settings.BOT_ADMIN_IDS
+
+
+def check_if_user_is_developer(user_id_telegram: int | str) -> bool:
+    """
+    Проверяет, является ли пользователь разработчиком.
+    """
+    return str(user_id_telegram) in settings.BOT_DEVELOPER_IDS
