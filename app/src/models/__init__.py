d24c1a23b94b847515da480d088e42fbf58c84ce
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-04-29 15:52:08 +0300

    Register Course, User

--- app/src/models/__init__.py
+++ app/src/models/__init__.py
@@ -1,7 +1,11 @@
 from app.src.models.chat import Chat
+from app.src.models.course import Course
 from app.src.models.poll import Poll
+from app.src.models.user import User
 
 __all__ = [
     'Chat',
+    'Course',
     'Poll',
+    'User',
 ]
