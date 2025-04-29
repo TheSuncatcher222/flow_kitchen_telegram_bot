52a6c6c370c6d523fc802b824407c553993e998c
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-04-29 15:41:56 +0300

    Fix bot_was_removed, refactor

--- app/src/telegram_bot/routers/chat_control.py
+++ app/src/telegram_bot/routers/chat_control.py
@@ -2,42 +2,24 @@ from aiogram import Router
 from aiogram.filters import (
     ChatMemberUpdatedFilter,
     KICKED,
+    LEFT,
     MEMBER,
 )
 from aiogram.types.chat_member_updated import ChatMemberUpdated
 
-from app.src.crud.async_crud.chat_async_crud import chat_async_crud
+from app.src.crud.chat import chat_crud
 from app.src.database.database import (
     RedisKeys,
     async_session_maker,
 )
-from app.src.utils.redis_data import redis_delete
+from app.src.utils.redis_app import redis_delete
 
-IF_KICKED = ChatMemberUpdatedFilter(member_status_changed=KICKED)
-IF_ADDED = ChatMemberUpdatedFilter(member_status_changed=MEMBER)
+IF_ADDED: ChatMemberUpdatedFilter = ChatMemberUpdatedFilter(member_status_changed=MEMBER)
+IF_KICKED: ChatMemberUpdatedFilter = ChatMemberUpdatedFilter(member_status_changed=KICKED)
+IF_LEFT: ChatMemberUpdatedFilter = ChatMemberUpdatedFilter(member_status_changed=LEFT)
 
 router: Router = Router()
 
-
-# TODO. Не работает.
-@router.my_chat_member(IF_KICKED)
-async def bot_was_kicked(event: ChatMemberUpdated) -> None:
-    """
-    Обрабатывает событие "бота исключили из чата".
-    """
-    with async_session_maker() as session:
-        chat_async_crud.delete_by_id(
-            obj_id=event.chat.id,
-            session=session,
-        )
-
-    for key in (
-        RedisKeys.CHAT_ALL_IDS,
-        RedisKeys.CHAT_ALL_TITLES,
-    ):
-        redis_delete(key=key)
-
-    return
 
 @router.my_chat_member(IF_ADDED)
 async def bot_was_added(event: ChatMemberUpdated) -> None:
@@ -45,17 +27,37 @@ async def bot_was_added(event: ChatMemberUpdated) -> None:
     Обрабатывает событие "бота добавил в чат".
     """
     async with async_session_maker() as session:
-        chat_id_str = str(event.chat.id)
-        await chat_async_crud.create(
-            obj_data={
-                'chat_id': chat_id_str,
-                'is_group': (
-                    True
-                    if chat_id_str.startswith('-')
-                    else False
-                ),
-                'title': event.chat.title,
-            },
+        chat_id_str: str = str(event.chat.id)
+        try:
+            await chat_crud.create(
+                obj_data={
+                    'chat_id': chat_id_str,
+                    'is_group': chat_id_str.startswith('-'),
+                    'title': event.chat.title,
+                },
+                session=session,
+            )
+        except ValueError as err:
+            if chat_crud.unique_columns_err != err.args[0]:
+                raise
+            return
+
+    for key in (
+        RedisKeys.CHAT_ALL_IDS,
+        RedisKeys.CHAT_ALL_TITLES,
+    ):
+        redis_delete(key=key)
+
+
+@router.my_chat_member(IF_KICKED)
+@router.my_chat_member(IF_LEFT)
+async def bot_was_removed(event: ChatMemberUpdated) -> None:
+    """
+    Обрабатывает событие "бота исключили/забанили из чата".
+    """
+    async with async_session_maker() as session:
+        await chat_crud.delete_by_chat_id(
+            chat_id=str(event.chat.id),
             session=session,
         )
 
@@ -64,5 +66,3 @@ async def bot_was_added(event: ChatMemberUpdated) -> None:
         RedisKeys.CHAT_ALL_TITLES,
     ):
         redis_delete(key=key)
-
-    return
