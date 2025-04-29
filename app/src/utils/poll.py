ade11de31cad465d0386ca71d72273057016fb8c
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-04-29 15:39:20 +0300

    Delete all, add get_all_polls_titles, poll_send, poll_block

--- app/src/utils/poll.py
+++ app/src/utils/poll.py
@@ -1,216 +1,105 @@
-import asyncio
-from datetime import (
-    date,
-    datetime,
-    time,
-    timedelta,
+from datetime import timedelta
+from typing import TYPE_CHECKING
+
+from aiogram.methods import (
+    SendPoll,
+    StopPoll,
 )
+from aiogram.types import Message
+from apscheduler.triggers.date import DateTrigger
 
-from app.src.config.config import (
-    TimeIntervals,
-    settings,
-)
-from app.src.crud.sync_crud.poll_sync_crud import poll_sync_crud
-from app.src.database.database import (
-    RedisKeys,
-    sync_session_maker,
-)
-from app.src.models.poll import Poll
-from app.src.utils.bot_send_poll import bot_send_poll
-from app.src.utils.bot_stop_poll import bot_stop_poll
-from app.src.utils.redis_data import (
-    redis_get,
-    redis_set,
-)
+from app.src.crud.poll import poll_crud
+from app.src.database.database import async_session_maker
+from app.src.scheduler.scheduler import scheduler
+from app.src.telegram_bot.bot import bot
+from app.src.utils.date import get_today_datetime_data
 
-def check_if_poll_is_needed_to_send(
-    now_time: time,
-    poll_data: dict[str, any],
-    today_date_str: str,
-    today_day_of_week: str,
-) -> tuple[bool, bool]:
+if TYPE_CHECKING:
+    from app.src.models.poll import Poll
+
+
+async def get_all_polls_titles() -> list[str]:
     """
-    Проверяет, нужно ли отправлять опрос.
-
-    Возвращает кортеж:
-        1) bool - нужно ли отправить
-        2) bool - нужно ли не отправлять, но пометить отправленным
+    Возвращает список всех названий опросов.
     """
-    if poll_data['last_send_date'] == today_date_str:
-        return False, False
-
-    if today_date_str in poll_data['dates_skip']:
-        return False, True
-
-    if (
-        today_day_of_week in poll_data['send_days_of_week_list']
-        and
-        now_time >= time.fromisoformat(poll_data['send_time'])
-    ):
-        return True, True
-
-    return False, False
+    async with async_session_maker() as session:
+        return [p.title for p in await poll_crud.retrieve_all(session=session)]
 
 
-def check_if_poll_is_needed_to_stop_answers(
-    now_time: time,
-    poll_data: dict[str, any],
-    today_date: date,
-) -> bool:
+async def poll_send(poll_id: int) -> None:
     """
-    Проверяет, нужно ли останавливать опрос.
+    Отправляет опрос в телеграм чат/группу.
     """
-    block_answer_delta_hours: int | None = poll_data['block_answer_delta_hours']
-
-    if (
-        not block_answer_delta_hours
-        or
-        poll_data['is_poll_is_blocked']
-        or
-        (
-            not poll_data['last_send_date']
-            or
-            poll_data['last_send_date'] == 'None'
-        )
-    ):
-        return False
-
-    last_send_datetime: datetime = datetime.combine(
-        date=date.fromisoformat(poll_data['last_send_date']),
-        time=time.fromisoformat(poll_data['send_time']),
-    )
-    now_datetime: datetime = datetime.combine(
-        date=today_date,
-        time=now_time,
-    )
-    if now_datetime > last_send_datetime + timedelta(hours=block_answer_delta_hours):
-        stop_poll(poll_data=poll_data)
-        return True
-
-    return False
-
-
-def get_all_polls() -> list[dict[str, any]]:
-    """
-    Возвращает список всех опросов из базы данных.
-    """
-    if not settings.DEBUG_POLL_CACHE:
-        all_polls: None | dict[list[dict[str, any]]] = redis_get(key=RedisKeys.POLL_ALL)
-        if isinstance(all_polls, dict) and 'all_polls' in all_polls:
-            return all_polls['all_polls']
-
-    with sync_session_maker() as session:
-        polls: list[Poll] = poll_sync_crud.retrieve_all(session=session)
-
-    if len(polls) == 0:
-        redis_set(
-            key=RedisKeys.POLL_ALL,
-            value={'all_polls': []},
+    async with async_session_maker() as session:
+        poll: Poll = await poll_crud.retrieve_by_id(
+            obj_id=poll_id,
+            session=session,
         )
 
-    all_polls: list[dict[str, any]] = parse_polls_to_redis(polls=polls)
-    redis_set(
-        key=RedisKeys.POLL_ALL,
-        value={'all_polls': all_polls},
-        ex_sec=TimeIntervals.SECONDS_IN_1_DAY,
-    )
+    now_datetime, now_today_date, now_today_date_str, *_ = get_today_datetime_data()
 
-    return all_polls
+    if poll.last_send_date == now_today_date_str:
+        return
 
+    obj_data: dict[str, any] = {'last_send_date': now_today_date}
 
-def send_poll(poll_data: dict[str, any]) -> int | None:
-    """
-    Отправляет опрос в телеграм чат.
-    """
-    loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
-    if loop.is_closed():
-        loop: asyncio.AbstractEventLoop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop=loop)
-
-    try:
-        poll_id: int = loop.run_until_complete(
-            bot_send_poll(
-                chat_id=poll_data['chat_id'],
-                question=poll_data['topic'],
-                options=poll_data['options'],
-                is_anonymous=poll_data['is_allows_anonymous_answers'],
-                allows_multiple_answers=poll_data['is_allows_multiple_answers'],
+    if now_today_date_str in poll.dates_skip:
+        obj_data.update({
+            'dates_skip': [d for d in poll.dates_skip if d != now_today_date_str],
+            'is_blocked': True,
+        })
+    else:
+        message: Message = await bot(
+            SendPoll(
+                chat_id=poll.chat_id,
+                question=poll.topic,
+                options=poll.options,
+                is_anonymous=poll.is_allows_anonymous_answers,
+                allows_multiple_answers=poll.is_allows_multiple_answers,
             ),
         )
-
-    except Exception as err:
-        # TODO. Обработать ошибку.
-        poll_id: None = None
-
-    finally:
-        # TODO. Отправить сообщение в телеграм чат.
-        pass
-
-    return poll_id
-
-
-def stop_poll(poll_data: dict[str, any]) -> None:
-    """
-    Останавливает опрос в телеграм чате.
-    """
-    loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
-    if loop.is_closed():
-        loop: asyncio.AbstractEventLoop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop=loop)
-
-    try:
-        loop.run_until_complete(
-            bot_stop_poll(poll_data=poll_data),
+        obj_data.update({
+            'is_blocked': False,
+            'message_id': str(message.message_id),
+        })
+        scheduler.add_job(
+            id=f'Block poll id={poll.id}, day={1}',
+            trigger=DateTrigger(run_date=now_datetime + timedelta(hours=poll.block_answer_delta_hours)),
+            func=poll_block,
+            kwargs={'poll_id': poll.id},
         )
 
-    except Exception as err:
-        # TODO. Обработать ошибку.
-        pass
-
-    finally:
-        # TODO. Отправить сообщение в телеграм чат.
-        pass
-
-    return
-
-
-def mark_poll_as_sended_and_unblocked(
-    poll_data: dict[str, any],
-    message_id: int,
-    today_date: date,
-) -> None:
-    with sync_session_maker() as session:
-        poll: Poll = poll_sync_crud.retrieve_by_id(
-            obj_id=poll_data['id'],
+    async with async_session_maker() as session:
+        await poll_crud.update_by_id(
+            obj_id=poll_id,
+            obj_data=obj_data,
             session=session,
         )
-        dates_skip: list[str] = [date_skip for date_skip in poll.dates_skip if date_skip > str(today_date)]
-        dates_skip.sort()
 
-        poll_sync_crud.update_by_id(
-            obj_id=poll_data['id'],
-            obj_data={
-                'last_send_date': today_date,
-                'message_id': message_id,
-                'is_poll_is_blocked': False,
-                'dates_skip': dates_skip,
-            },
+
+async def poll_block(poll_id: int) -> None:
+    """
+    Блокирует опрос.
+    """
+    async with async_session_maker() as session:
+        poll: Poll = await poll_crud.retrieve_by_id(
+            obj_id=poll_id,
             session=session,
-            perform_check_unique=False,
-            perform_cleanup=False,
         )
-    return
 
+    if poll.is_blocked:
+        return
 
-def parse_polls_to_redis(polls: list[Poll]) -> list[dict[str, any]]:
-    """
-    Преобразует тип данных list[Poll] в list[dict[str, any]].
-    """
-    return [
-        poll.to_dict_repr(
-            represent_date_as_str=True,
-            represent_time_as_str=True,
+    await bot(
+        StopPoll(
+            chat_id=poll.chat_id,
+            message_id=poll.message_id,
+        ),
+    )
+
+    async with async_session_maker() as session:
+        await poll_crud.update_by_id(
+            obj_id=poll.id,
+            obj_data={'is_blocked': True},
+            session=session,
         )
-        for poll
-        in polls
-    ]
