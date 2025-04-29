f58b6413ad7bfdbba220edd85a1e49885a7cb216
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-04-29 15:34:18 +0300

    Refactor

--- app/src/utils/translation.py
+++ app/src/utils/translation.py
@@ -1,23 +1,32 @@
+DAYS_RUS_TO_ENG_CRON: dict[str, str] = {
+    'пн': 'mon',
+    'вт': 'tue',
+    'ср': 'wed',
+    'чт': 'thu',
+    'пт': 'fri',
+    'сб': 'sat',
+    'вс': 'sun',
+}
+DAYS_ENG_CRON_TO_RUS: dict[str, str] = {
+    'mon': 'пн',
+    'tue': 'вт',
+    'wed': 'ср',
+    'thu': 'чт',
+    'fri': 'пт',
+    'sat': 'сб',
+    'sun': 'вс',
+}
 
-from app.src.validators.poll import (
-    DAYS_RUS_TO_ENG_CRON,
-    DAYS_ENG_CRON_TO_RUS,
-)
 
-
-def translate_days_of_week_from_rus_to_eng(
-    send_days_of_week_list: list[str],
-) -> list[str]:
+def translate_days_of_week_from_rus_to_eng(days: list[str]) -> list[str]:
     """
     Преобразует дни недели из русского в английский.
     """
-    return [DAYS_RUS_TO_ENG_CRON[day.lower()] for day in send_days_of_week_list]
+    return [DAYS_RUS_TO_ENG_CRON[d.lower()] for d in days]
 
 
-def translate_days_of_week_from_eng_to_rus(
-    send_days_of_week_list: list[str],
-) -> list[str]:
+def translate_days_of_week_from_eng_to_rus(days: list[str]) -> list[str]:
     """
     Преобразует дни недели из английского в русский.
     """
-    return [DAYS_ENG_CRON_TO_RUS[day.lower()] for day in send_days_of_week_list]
+    return [DAYS_ENG_CRON_TO_RUS[d.lower()] for d in days]
