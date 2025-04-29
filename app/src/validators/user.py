c03d4df4cf3f8b7cd6e538d89065559c74714606
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-04-29 15:53:59 +0300

    Add UserParams

---
+++ app/src/validators/user.py
@@ -0,0 +1,10 @@
+class UserParams:
+    """
+    Параметры пользователей.
+    """
+
+    # INFO. Идентификатор меньше, чем 52 бита (4.5e+15) + символ '-'
+    ID_TELEGRAM_LEN_MAX: int = 16 + 1
+    NAME_FIRST_LEN_MAX: int = 64
+    NAME_LAST_LEN_MAX: int = 64
+    USERNAME_LEN_MAX: int = 32
